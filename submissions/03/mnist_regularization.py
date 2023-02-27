826e958ac9869a8861b8fabd6fe23fcc312e42a2
Jakub Peleška <pelesjak@fel.cvut.cz>
2023-02-27 23:49:48 +0100

    Add templates for labs 03 and solution to mnist_regularization

---
+++ submissions/03/mnist_regularization.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+import re
+from typing import Dict
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
+
+import numpy as np
+import tensorflow as tf
+
+from mnist import MNIST
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
+parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
+parser.add_argument("--dropout", default=0, type=float, help="Dropout regularization.")
+parser.add_argument("--epochs", default=30, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layers", default=[400], nargs="*", type=int, help="Hidden layer sizes.")
+parser.add_argument("--label_smoothing", default=0, type=float, help="Label smoothing.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--seed", default=42, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--weight_decay", default=0, type=float, help="Weight decay strength.")
+# If you add more arguments, ReCodEx will keep them with your default values.
+
+
+def main(args: argparse.Namespace) -> Dict[str, float]:
+    # Set the random seed and the number of threads.
+    tf.keras.utils.set_random_seed(args.seed)
+    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+    if args.debug:
+        tf.config.run_functions_eagerly(True)
+        tf.data.experimental.enable_debug_mode()
+
+    # Create logdir name
+    args.logdir = os.path.join("logs", "{}-{}-{}".format(
+        os.path.basename(globals().get("__file__", "notebook")),
+        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
+    ))
+
+    # Load data
+    mnist = MNIST(size={"train": 5000})
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=[MNIST.H, MNIST.W, MNIST.C]))
+    # Add dropout for regularization
+    model.add(tf.keras.layers.Dropout(args.dropout))
+    for hidden_layer in args.hidden_layers:
+        model.add(tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu))
+        # Add dropout for regularization
+        model.add(tf.keras.layers.Dropout(args.dropout))
+    model.add(tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax))
+
+    # Transform labels to categorical soft labels
+    mnist.train.data["labels"] = tf.keras.utils.to_categorical(mnist.train.data["labels"])
+    mnist.test.data["labels"] = tf.keras.utils.to_categorical(mnist.test.data["labels"])
+    mnist.dev.data["labels"] = tf.keras.utils.to_categorical(mnist.dev.data["labels"])
+    
+    optimizer = tf.optimizers.experimental.AdamW(weight_decay=args.weight_decay)
+    optimizer.exclude_from_weight_decay(var_names=["bias"])
+
+    model.compile(
+        optimizer=optimizer,
+        loss=tf.losses.CategoricalCrossentropy(label_smoothing=args.label_smoothing),
+        metrics=[tf.metrics.CategoricalAccuracy(name="accuracy")],
+    )
+
+    tb_callback = tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1)
+
+    logs = model.fit(
+        mnist.train.data["images"], mnist.train.data["labels"],
+        batch_size=args.batch_size, epochs=args.epochs,
+        validation_data=(mnist.dev.data["images"], mnist.dev.data["labels"]),
+        callbacks=[tb_callback],
+    )
+
+    # Return development metrics for ReCodEx to validate.
+    return {metric: values[-1] for metric, values in logs.history.items() if metric.startswith("val_")}
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
