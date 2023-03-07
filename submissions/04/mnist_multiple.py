57fb4822880dce8eb79559c9c292522272a3a807
Jakub Peleška <pelesjak@fel.cvut.cz>
2023-03-07 18:02:25 +0100

    Add solution to mnist_multiple

--- submissions/04/mnist_multiple.py
+++ submissions/04/mnist_multiple.py
@@ -4,62 +4,78 @@ import datetime
 import os
 import re
 from typing import Dict
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
+# Report only TF errors by default
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
 import numpy as np
 import tensorflow as tf
-
 from mnist import MNIST
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
+parser.add_argument("--debug", default=False, action="store_true",
+                    help="If given, run functions eagerly.")
 parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument("--recodex", default=False,
+                    action="store_true", help="Evaluation in ReCodEx.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--threads", default=1, type=int,
+                    help="Maximum number of threads to use.")
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 
 class Model(tf.keras.Model):
     def __init__(self, args: argparse.Namespace) -> None:
         # Create a model with two inputs, both images of size [MNIST.H, MNIST.W, MNIST.C].
-        images = (
-            tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C]),
-            tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C]),
-        )
+        inputs = (tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C]),
+                  tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C]))
 
-        # TODO: The model starts by passing each input image through the same
-        # subnetwork (with shared weights), which should perform
-        # - convolution with 10 filters, 3x3 kernel size, stride 2, "valid" padding, ReLU activation,
-        # - convolution with 20 filters, 3x3 kernel size, stride 2, "valid" padding, ReLU activation,
-        # - flattening layer,
-        # - fully connected layer with 200 neurons and ReLU activation,
-        # obtaining a 200-dimensional feature vector FV of each image.
+        next_input1 = inputs[0]
+        next_input2 = inputs[1]
 
-        # TODO: Using the computed representations, the model should produce four outputs:
-        # - first, compute _direct comparison_ whether the first digit is
-        #   greater than the second, by
-        #   - concatenating the two 200-dimensional image representations FV,
-        #   - processing them using another 200-neuron ReLU dense layer
-        #   - computing one output using a dense layer with `tf.nn.sigmoid` activation
-        # - then, classify the computed representation FV of the first image using
-        #   a densely connected softmax layer into 10 classes;
-        # - then, classify the computed representation FV of the second image using
-        #   the same layer (identical, i.e., with shared weights) into 10 classes;
-        # - finally, compute _indirect comparison_ whether the first digit
-        #   is greater than second, by comparing the predictions from the above
-        #   two outputs.
+        conv1_layer = tf.keras.layers.Conv2D(
+            10, 3, 2, "valid", activation=tf.nn.relu)
+        next_input1 = conv1_layer(next_input1)
+        next_input2 = conv1_layer(next_input2)
+
+        conv2_layer = tf.keras.layers.Conv2D(
+            20, 3, 2, "valid", activation=tf.nn.relu)
+        next_input1 = conv2_layer(next_input1)
+        next_input2 = conv2_layer(next_input2)
+
+        flatten = tf.keras.layers.Flatten()
+        next_input1 = flatten(next_input1)
+        next_input2 = flatten(next_input2)
+
+        dense1_layer = tf.keras.layers.Dense(200, activation=tf.nn.relu)
+        next_input1 = dense1_layer(next_input1)
+        next_input2 = dense1_layer(next_input2)
+
+        shared_outputs = [next_input1, next_input2]
+
+        next_inputs = tf.keras.layers.Concatenate()(shared_outputs)
+        next_inputs = tf.keras.layers.Dense(
+            200, activation=tf.nn.relu)(next_inputs)
+        direct_comparison = tf.keras.layers.Dense(
+            1, activation=tf.nn.sigmoid)(next_inputs)
+
+        digit_classifier = tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+        digit_1 = digit_classifier(shared_outputs[0])
+        digit_2 = digit_classifier(shared_outputs[1])
+
+        indirect_comparison = tf.argmax(
+            digit_1, axis=1) > tf.argmax(digit_2, axis=1)
+
         outputs = {
-            "direct_comparison": ...,
-            "digit_1": ...,
-            "digit_2": ...,
-            "indirect_comparison": ...,
+            "direct_comparison": direct_comparison,
+            "digit_1": digit_1,
+            "digit_2": digit_2,
+            "indirect_comparison": indirect_comparison,
         }
 
         # Finally, construct the model.
-        super().__init__(inputs=images, outputs=outputs)
+        super().__init__(inputs=inputs, outputs=outputs)
 
         # Note that for historical reasons, names of a functional model outputs
         # (used for displayed losses/metric names) are derived from the name of
@@ -67,21 +83,16 @@ class Model(tf.keras.Model):
         # the keys of the `outputs` dictionary.
         self.output_names = sorted(outputs.keys())
 
-        # TODO: Define the appropriate losses for the model outputs
-        # "direct_comparison", "digit_1", "digit_2". Regarding metrics,
-        # the accuracy of both the direct and indirect comparisons should be
-        # computed; name both metrics "accuracy" (i.e., pass "accuracy" as the
-        # first argument of the metric object).
         self.compile(
             optimizer=tf.keras.optimizers.Adam(jit_compile=False),
             loss={
-                "direct_comparison": ...,
-                "digit_1": ...,
-                "digit_2": ...,
+                "direct_comparison": tf.keras.losses.BinaryCrossentropy(),
+                "digit_1": tf.keras.losses.SparseCategoricalCrossentropy(),
+                "digit_2": tf.keras.losses.SparseCategoricalCrossentropy(),
             },
             metrics={
-                "direct_comparison": [...],
-                "indirect_comparison": [...],
+                "direct_comparison": [tf.keras.metrics.BinaryAccuracy("accuracy")],
+                "indirect_comparison": [tf.keras.metrics.BinaryAccuracy("accuracy")],
             },
         )
         self.tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
@@ -91,22 +102,21 @@ class Model(tf.keras.Model):
         self, mnist_dataset: MNIST.Dataset, args: argparse.Namespace, training: bool = False
     ) -> tf.data.Dataset:
         # Start by using the original MNIST data
-        dataset = tf.data.Dataset.from_tensor_slices((mnist_dataset.data["images"], mnist_dataset.data["labels"]))
+        dataset = tf.data.Dataset.from_tensor_slices(
+            (mnist_dataset.data["images"], mnist_dataset.data["labels"]))
 
-        # TODO: If `training`, shuffle the data with `buffer_size=10000` and `seed=args.seed`.
+        if training:
+            dataset = dataset.shuffle(buffer_size=10000, seed=args.seed)
 
-        # TODO: Combine pairs of examples by creating batches of size exactly 2.
+        dataset = dataset.batch(2, drop_remainder=True)
 
-        # TODO: Map pairs of images to elements suitable for our model. Notably,
-        # the elements should be pairs `(input, output)`, with
-        # - `input` being a pair of images,
-        # - `output` being a dictionary with keys "digit_1", "digit_2", "direct_comparison",
-        #   and "indirect_comparison".
         def create_element(images, labels):
-            ...
-        dataset = dataset.map(create_element)
+            return (images[0], images[1]), {"direct_comparison": labels[0] > labels[1],
+                                            "digit_1": labels[0],
+                                            "digit_2": labels[1],
+                                            "indirect_comparison": labels[0] > labels[1]}
 
-        # TODO: Create batches of size `args.batch_size`
+        dataset = dataset.map(create_element).batch(args.batch_size)
 
         return dataset
 
@@ -124,7 +134,8 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     args.logdir = os.path.join("logs", "{}-{}-{}".format(
         os.path.basename(globals().get("__file__", "notebook")),
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
+        ",".join(("{}={}".format(
+            re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
     ))
 
     # Load the data
@@ -138,7 +149,8 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     dev = model.create_dataset(mnist.dev, args)
 
     # Train
-    logs = model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[model.tb_callback])
+    logs = model.fit(train, epochs=args.epochs,
+                     validation_data=dev, callbacks=[model.tb_callback])
 
     # Return development metrics for ReCodEx to validate.
     return {metric: values[-1] for metric, values in logs.history.items() if metric.startswith("val_")}
