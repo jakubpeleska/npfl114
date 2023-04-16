ef646c9dfe1df27762f3d3eb98086be97277b282
Jakub Peleska <pelesjak@fel.cvut.cz>
2023-04-16 20:04:23 +0200

    Add 3D recognition solution

--- submissions/09/3d_recognition.py
+++ submissions/09/3d_recognition.py
@@ -1,3 +1,5 @@
+# 8a939b25-3475-4096-a653-2d836d1cbcad
+# a0e27015-19ae-4c8f-81bd-bf628505a35a
 #!/usr/bin/env python3
 import argparse
 import datetime
@@ -9,16 +11,33 @@ import numpy as np
 import tensorflow as tf
 
 from modelnet import ModelNet
+from cnn_model import CNNModel
 
-# TODO: Define reasonable defaults and optionally more parameters.
-# Also, you can set the number of threads to 0 to use all your CPU cores.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
+parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
 parser.add_argument("--debug", default=False, action="store_true", help="If given, run functions eagerly.")
-parser.add_argument("--epochs", default=..., type=int, help="Number of epochs.")
+parser.add_argument("--epochs", default=25, type=int, help="Number of epochs.")
 parser.add_argument("--modelnet", default=20, type=int, help="ModelNet dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--weights_file", default="weights.h5", type=str, help="Name of file for saving the trained weights.")
+
+def process_data(modelnet: ModelNet, args: argparse.Namespace):
+    train = tf.data.Dataset.from_tensor_slices((modelnet.train.data["voxels"], modelnet.train.data["labels"]))
+    dev = tf.data.Dataset.from_tensor_slices((modelnet.dev.data["voxels"], modelnet.dev.data["labels"]))
+    test = tf.data.Dataset.from_tensor_slices(modelnet.test.data["voxels"])
+    
+    # Use only the first 5000 images, shuffle them and change type from tf.uint8 to tf.float32.
+    train = train.shuffle(5000, seed=args.seed)
+    
+    # It allows the pipeline to run in parallel with
+    # the training process, dynamically adjusting the buffer size of the
+    # prefetched elements.
+    train = train.batch(args.batch_size).prefetch(tf.data.AUTOTUNE)
+    dev = dev.batch(args.batch_size).prefetch(tf.data.AUTOTUNE)
+    test = test.batch(args.batch_size).prefetch(tf.data.AUTOTUNE)
+    
+    return train, dev, test
 
 
 def main(args: argparse.Namespace) -> None:
@@ -39,15 +58,78 @@ def main(args: argparse.Namespace) -> None:
 
     # Load the data
     modelnet = ModelNet(args.modelnet)
+    
+    train, dev, test = process_data(modelnet, args)
 
-    # TODO: Create the model and train it
-    model = ...
+    # Model architecture scale
+    scale = 1
+    
+    # VGG16-like model
+    conv1_filters = int(32*scale)
+    conv1_repeat = 1
+    conv1 = f'CB-{conv1_filters}-3-1-same,' * conv1_repeat
+    max_pool1 = f'M-2-2'
+    
+    block1 = f'{conv1}{max_pool1}'
+    
+    conv2_filters = int(64*scale)
+    conv2_repeat = 1
+    conv2 = f'CB-{conv2_filters}-3-1-same,' * conv2_repeat
+    max_pool2 = f'M-2-2'
+    
+    block2 = f'{conv2}{max_pool2}'
+    
+    conv3_filters = int(128*scale)
+    conv3_repeat = 2
+    conv3 = f'CB-{conv3_filters}-3-1-same,' * conv3_repeat
+    max_pool3 = f'M-2-2'
+    
+    block3 = f'{conv3}{max_pool3}'
+
+    conv4_filters = int(256*scale)
+    conv4_repeat = 2
+    conv4 = f'CB-{conv4_filters}-3-1-same,' * conv4_repeat
+    max_pool4 = f'M-2-2'
+    
+    block4 = f'{conv4}{max_pool4}'
+    
+    N1 = int(256*scale)
+    N2 = int(128*scale)
+    dropout = 0.5
+    lin_class = f'F,H-{N1},D-{dropout},H-{N2},D-{dropout}'
+    
+    # Generate test set annotations, but in `args.logdir` to allow parallel execution.
+    os.makedirs(args.logdir, exist_ok=True)
+
+    # Create model
+    model = CNNModel([modelnet.D, modelnet.H, modelnet.W, modelnet.C], len(modelnet.LABELS),
+                     f"{block1},{block2},{block3},{lin_class}", 
+                     dim=3, logdir=args.logdir)
+    
+    # model.summary()
+    
+    weights_file = args.weights_file
+    
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(filepath=weights_file,
+                                                      monitor='val_accuracy',
+                                                      mode='max',
+                                                      save_weights_only=True,
+                                                      save_best_only=True)
+    
+    # Learn model
+    model.fit(train, 
+              epochs=args.epochs, 
+              validation_data=dev,
+              callbacks=[model.tb_callback, checkpoint_cb]
+              )
+    
+    model.load_weights(weights_file, True)
 
     # Generate test set annotations, but in `args.logdir` to allow parallel execution.
     os.makedirs(args.logdir, exist_ok=True)
     with open(os.path.join(args.logdir, "3d_recognition.txt"), "w", encoding="utf-8") as predictions_file:
         # TODO: Predict the probabilities on the test set
-        test_probabilities = model.predict(...)
+        test_probabilities = model.predict(test)
 
         for probs in test_probabilities:
             print(np.argmax(probs), file=predictions_file)
---
+++ submissions/09/cnn_model.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+import os
+import re
+
+from typing import Iterable
+# Report only TF errors by default
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
+
+import tensorflow as tf
+
+class CNNModel(tf.keras.Model):
+    """ 
+    Abstraction model class for creating Convolutional Neural Networks.
+    """
+  
+    def __init__(self, input_shape: Iterable[int], out_shape: int, architecture_instructions: str, dim = 2, logdir: str = None):
+        """Abstraction model class for creating Convolutional Neural Networks.
+
+        Args:
+            input_shape (Shape): Input shape.
+            out_shape (Shape): Output shape.
+            architecture_instructions (str): Instructions for creating the architecture of CNN model.
+            dim (int, optional): Dimension of convolution layers.
+            logdir (str, optional): Tensorboard logging directory if not specified Tesorboard is disabled. Defaults to None.
+            --- 
+            
+            Specification of architecture_instructions:
+            
+            architecture_instructions argument, contains comma-separated specifications of the following layers:
+                * C-(filters)-(kernelsize)-(stride)-(padding): Add a convolution layer with ReLU activation and 
+                    specified number of filters, kernel size, stride and padding. 
+                Example: C-10-3-1-same
+                    
+                * CB-(filters)-(kernelsize)-(stride)-(padding): Same as C-(filters)-(kernelsize)-(stride)-(padding), 
+                    but use batch normalization. In detail, start with a convolution layer without bias and activation,
+                    then add batch normalization layer, and finally ReLU activation.     
+                Example: CB-10-3-1-same
+                
+                * M-(poolsize)-(stride): Add max pooling with specified size and stride, using the default "valid" padding. 
+                Example: M-3-2
+                
+                * R-(layers): Add a residual connection. The layers contain a specification of at least one 
+                    convolution layer (but not a recursive residual connection R). 
+                    The input to the R layer should be processed sequentially by layers, 
+                    and the produced output (after the ReLU non-linearity of the last layer) should
+                    be added to the input (of this R layer). 
+                Example: R-[C-16-3-1-same,C-16-3-1-same]
+                
+                * F: Flatten inputs. Must appear exactly once in the architecture.
+                
+                * H-(hiddenlayersize): Add a dense layer with ReLU activation and specified size. 
+                Example: H-100
+                
+                * D-(dropoutrate): Apply dropout with the given dropout rate. 
+                Example: D-0.5
+                
+            An example architecture might be --cnn=CB-16-5-2-same,M-3-2,F,H-100,D-0.5. You can assume the resulting network is valid; it is fine to crash if it is not.
+
+        """
+        inputs = tf.keras.layers.Input(shape=input_shape)
+
+        next_inputs = inputs
+        cnn_architecture = self.parse_architecture(architecture_instructions)
+        
+        if dim == 1:
+          self.conv = tf.keras.layers.Convolution1D
+          self.max_pool = tf.keras.layers.MaxPool1D
+        elif dim == 2:
+          self.conv = tf.keras.layers.Convolution2D
+          self.max_pool = tf.keras.layers.MaxPool2D
+        elif dim == 3:
+          self.conv = tf.keras.layers.Convolution3D
+          self.max_pool = tf.keras.layers.MaxPool3D
+        else:
+          raise ValueError('dim argument can have values 1, 2 or 3')
+
+        for layer_type, parameters in cnn_architecture:
+            next_inputs = self.apply_layer(next_inputs, layer_type, parameters)
+
+        hidden = next_inputs
+
+        # Add the final output layer
+        outputs = tf.keras.layers.Dense(out_shape, activation=tf.nn.softmax)(hidden)
+
+        super().__init__(inputs=inputs, outputs=outputs)
+        self.compile(
+            optimizer=tf.optimizers.Adam(jit_compile=False),
+            loss=tf.losses.SparseCategoricalCrossentropy(),
+            metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        )
+        if logdir is not None:
+            self.tb_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+        
+            
+    def apply_layer(self, inputs: list, layer_type: str, parameters: list):
+        if layer_type == 'C':
+            filters, kernel_size, stride, padding = parameters
+            return self.conv(
+                int(filters), int(kernel_size), int(stride), padding, activation=tf.nn.relu)(inputs)
+
+        elif layer_type == 'CB':
+            filters, kernel_size, stride, padding = parameters
+            conv_outputs = self.conv(int(filters), int(kernel_size), int(stride), padding, use_bias=False)(inputs)
+            batch_norm_outputs = tf.keras.layers.BatchNormalization()(conv_outputs)
+            return tf.keras.activations.relu(batch_norm_outputs)
+
+        elif layer_type == 'M':
+            pool_size, stride = parameters
+            return self.max_pool(int(pool_size), int(stride))(inputs)
+
+        elif layer_type == 'R':
+            res_block_instructions = parameters[0].replace('[','').replace(']', '')
+            res_block_architecture = self.parse_architecture(res_block_instructions)
+            next_inputs = inputs
+            for res_layer_type, res_parameters in res_block_architecture:
+                next_inputs = self.apply_layer(next_inputs, res_layer_type, res_parameters)
+            return next_inputs + inputs
+
+        elif layer_type == 'F':
+            return tf.keras.layers.Flatten()(inputs)
+
+        elif layer_type == 'H':
+            [hidden_layer_size] = parameters
+            return tf.keras.layers.Dense(int(hidden_layer_size), activation=tf.nn.relu)(inputs)
+
+        elif layer_type == 'D':
+            [dropout_rate] = parameters
+            return tf.keras.layers.Dropout(float(dropout_rate))(inputs)
+            
+        else:
+            raise ValueError(
+                f'Unknown layer type "{layer_type}" found!')
+    
+    def parse_architecture(self, instructions_str: str):
+        instructions = []
+        
+        instructions_parts = re.split(r",(?!(?:[^,\[\]]+,)*[^,\[\]]+])", instructions_str, 0)
+
+        for ins_part in instructions_parts:
+            [layer_type, *parameters] = re.split(r"-(?!(?:[^-\[\]]+-)*[^-\[\]]+])", ins_part, 0)
+            instructions.append((layer_type, parameters))
+
+        return instructions
+    
