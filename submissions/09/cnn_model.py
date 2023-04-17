7e9c2576706bd695c87d902e38d9420bbe052d88
Jakub Peleska <pelesjak@fel.cvut.cz>
2023-04-17 22:01:24 +0200

    Add random rotation

--- submissions/09/cnn_model.py
+++ submissions/09/cnn_model.py
@@ -83,11 +83,7 @@ class CNNModel(tf.keras.Model):
         outputs = tf.keras.layers.Dense(out_shape, activation=tf.nn.softmax)(hidden)
 
         super().__init__(inputs=inputs, outputs=outputs)
-        self.compile(
-            optimizer=tf.optimizers.Adam(jit_compile=False),
-            loss=tf.losses.SparseCategoricalCrossentropy(),
-            metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")],
-        )
+        
         if logdir is not None:
             self.tb_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
         
